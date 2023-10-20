82111a27637c53e9cb1f80c5f8197e4276f48ddd
Polaski <vainferro@gmail.com>
2023-10-20 11:58:23 +0300

    Cancelled project1 removal.

---
+++ 4. Watermelon/Watermelon divider/Watermelon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    int size;
+    cin >> size;
+    if (size == 2)
+    {
+        cout << "NO";
+    } else if( size % 2 == 0) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
---
+++ 5. Factorialize a number/Factorializer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    cout << "Provide a number to factorialize:"
+    return 0;
+}
