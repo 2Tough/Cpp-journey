9aeaffe63a61cb7d436b737eb7aeaef1187ac38d
Polaski <vainferro@gmail.com>
2023-10-20 12:15:47 +0300

    Finished factorializer.

--- 5. Factorialize a number/Factorializer.cpp
+++ 5. Factorialize a number/Factorializer.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 
 int main() {
-    
+    int number{};
     cout << "Provide a number to factorialize:"
+    cin >> number;
+    for (int i = 1; i <= number; i++) {
+        number *= i;
+    }
+    cout << "The factorialized number is: " + number;
+        
+
     return 0;
 }
--- 5. Factorialize a number/Project1.cpp
+++ 5. Factorialize a number/Project1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello World!";
+    int number, sum = 1;
+    cout << "Provide a number to factorialize:";
+    cin >> number;
+
+    for (int i = 1; i < number; i++) {
+        sum *= i+1;
+    }
+    cout << "The factorialized number is:";
+    cout << sum;
     return 0;
 }
