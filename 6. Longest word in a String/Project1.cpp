7ddd933fe03822325fdd28d5d86bfaa69c5103c1
Polaski <vainferro@gmail.com>
2023-10-20 13:01:43 +0300

    Researching methods on how to split the string and include it in array.

---
+++ 6. Longest word in a String/Project1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    string sentenceArray[100];
+    string word;
+    cout << "Enter a sentence:";
+    getline(cin,word);
+    
+    cout << "Longest word is:";
+    
+     
+    return 0;
+}
